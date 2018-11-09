766015702756e7f9c06bb5fce70b9bcf75848a87
Chang Kim <dfrontier1@gmail.com>
2018-11-08 16:30:42 -0800

    functional and spits out correct label

--- knn.py
+++ knn.py
@@ -11,7 +11,7 @@ def distance(x,y,l):
             dist += abs(x[i]-y[i]) 
     elif l == 'L2':
         for i in range(4):
-            dist += (x[i]*y[i])**2
+            dist += (x[i]-y[i])**2
             dist = math.sqrt(dist)
     else:
         highest = abs(x[3]-y[3])
@@ -25,30 +25,32 @@ def KNN(train,K,test,l):
     S = {}
     for n in range(len(train)):
         S[distance(train[n],test,l)] = train[n][4]
-    sort = sorted(S.items(), key=lambda x: x[1])
+    sort = sorted(S.items(), key=lambda x: x[0])
     label = 0
-    for k in range(1,K+1):
-        nn = S[0:k]
-        for n in nn:
-            label += S.get(n)
-    return label
+    nn = sort[0:K]
+    for n in nn:
+        label += S.get(n[0])
+    if label < 0:
+        return -1
+    else:
+        return 1
 
-def parse_args(): # DONE
+def parse_args():
     parser = argparse.ArgumentParser(description = 'Implements KNN and takes in K and distance metric')
-    parser = add_argument('--K', type=int, help = 'number of nearest neighbors')
-    parser = add_argument('--method', help = 'distance metric: L1, L2, or Linf')
+    parser.add_argument('--K', type=int, help = 'number of nearest neighbors')
+    parser.add_argument('--method', help = 'distance metric: L1, L2, or Linf')
     return parser.parse_args()
 
-def parse_csv(csv): #DONE
-    df = pd.read_csv(csv,delimter='\t')
-    lists = [list(x) for in df.values]
+def parse_csv(csv):
+    df = pd.read_csv(csv)
+    lists = [list(x) for x in df.values]
     return lists
 
 def main():
     opts = parse_args()
     train = parse_csv('knn_train.csv')
     test = parse_csv('knn_test.csv')
-    for i in range(len(test))
+    for i in range(len(test)):
         print(KNN(train,opts.K,test[i],opts.method))
     
 if __name__ == '__main__':
